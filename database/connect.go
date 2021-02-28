aa70d516e783b7d88b48cf0503879e57c9aaaa1e
Hiram G <hiramg90@gmail.com>
2021-02-28 10:19:30 -0600

    enabling migrations

--- database/connect.go
+++ database/connect.go
@@ -1,17 +1,20 @@
 package database
 
 import (
+	"admin/models"
 	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func Connect() {
-	_, err := gorm.Open(mysql.Open("root:root@/go_admin"), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open("root:root@/go_admin"), &gorm.Config{})
 
 	if err != nil {
 		panic("Couldn't connect to database")
 	}
 
 	fmt.Println("Database connected!")
+
+	db.AutoMigrate(&models.User{})
 }
