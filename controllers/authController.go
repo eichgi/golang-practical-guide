d4786ba3674133e4775976d69de38f1233eedee2
Hiram G <hiramg90@gmail.com>
2021-02-28 19:14:10 -0600

    Creating Login function

--- controllers/authController.go
+++ controllers/authController.go
@@ -36,3 +36,31 @@ func Register(c *fiber.Ctx) error {
 	//return c.SendString("Hello, World 👋!")
 	return c.JSON(user)
 }
+
+func Login(c *fiber.Ctx) error {
+	var data map[string]string
+
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+
+	var user models.User
+
+	database.DB.Where("email = ?", data["email"]).First(&user)
+
+	if user.Id == 0 {
+		c.Status(404)
+		return c.JSON(fiber.Map{
+			"message": "User not found",
+		})
+	}
+
+	if err := bcrypt.CompareHashAndPassword(user.Password, []byte(data["password"])); err != nil {
+		c.Status(400)
+		return c.JSON(fiber.Map{
+			"message": "Incorrect password",
+		})
+	}
+
+	return c.JSON(user)
+}
