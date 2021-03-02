701275f9d529779fe2e4a36631df0d1b32d3f490
Hiram G <hiramg90@gmail.com>
2021-03-01 21:11:33 -0600

    Setting up cookie with jwt token; adding cors middleware to allow requests from frontend

--- controllers/authController.go
+++ controllers/authController.go
@@ -66,7 +66,7 @@ func Login(c *fiber.Ctx) error {
 	}
 
 	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
-		Issuer: strconv.Itoa(int(user.Id)),
+		Issuer:    strconv.Itoa(int(user.Id)),
 		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(), //Expires in 1 day
 	})
 
@@ -76,5 +76,16 @@ func Login(c *fiber.Ctx) error {
 		c.SendStatus(fiber.StatusInternalServerError)
 	}
 
-	return c.JSON(token)
+	cookie := fiber.Cookie{
+		Name:     "jwt",
+		Value:    token,
+		Expires:  time.Now().Add(time.Hour * 24),
+		HTTPOnly: true,
+	}
+
+	c.Cookie(&cookie)
+
+	return c.JSON(fiber.Map{
+		"message": "success",
+	})
 }
