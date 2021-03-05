0e35a09b7ddb2bfb80edf4ad54b476078508a52d
Hiram G <hiramg90@gmail.com>
2021-03-05 11:30:32 -0600

    Creating Users crud

--- controllers/userController.go
+++ controllers/userController.go
@@ -4,6 +4,7 @@ import (
 	"admin/database"
 	"admin/models"
 	"github.com/gofiber/fiber"
+	"strconv"
 )
 
 func AllUsers(c *fiber.Ctx) error {
@@ -15,10 +16,10 @@ func AllUsers(c *fiber.Ctx) error {
 	return c.JSON(users)
 }
 
-func CreateUser(c *fiber.Ctx) error  {
+func CreateUser(c *fiber.Ctx) error {
 	var user models.User
 
-	if err:= c.BodyParser(&user); err != nil {
+	if err := c.BodyParser(&user); err != nil {
 		return err
 	}
 
@@ -29,4 +30,49 @@ func CreateUser(c *fiber.Ctx) error  {
 	database.DB.Create(&user)
 
 	return c.JSON(user)
+}
+
+func GetUser(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+
+	user := models.User{
+		Id: uint(id),
+	}
+
+	database.DB.Find(&user)
+
+	return c.JSON(user)
+}
+
+func UpdateUser(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+
+	user := models.User{
+		Id: uint(id),
+	}
+
+	if err := c.BodyParser(&user); err != nil {
+		return err
+	}
+
+	database.DB.Model(&user).Updates(user)
+
+	return c.JSON(fiber.Map{
+		"message": "The user was updated",
+		"user":    user,
+	})
+}
+
+func DeleteUser(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+
+	user := models.User{
+		Id: uint(id),
+	}
+
+	database.DB.Delete(&user)
+
+	return c.JSON(fiber.Map{
+		"message": "The user was deleted",
+	})
 }
--- routes/routes.go
+++ routes/routes.go
@@ -14,8 +14,12 @@ func Setup(app *fiber.App) {
 
 	app.Post("/api/logout", controllers.Logout)
 	app.Get("/api/user", controllers.User)
+
 	app.Get("/api/users", controllers.AllUsers)
 	app.Post("/api/users", controllers.CreateUser)
+	app.Get("/api/users/:id", controllers.GetUser)
+	app.Put("/api/users/:id", controllers.UpdateUser)
+	app.Delete("/api/users/:id", controllers.DeleteUser)
 
 	app.Get("/other", controllers.Other)
 }
