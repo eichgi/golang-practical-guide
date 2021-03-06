47559367cc6159748a76eae160a348a4fce3ab00
Hiram G <hiramg90@gmail.com>
2021-03-05 18:21:12 -0600

    Generating relation between Role and User models

--- controllers/authController.go
+++ controllers/authController.go
@@ -28,6 +28,7 @@ func Register(c *fiber.Ctx) error {
 		FirstName: data["first_name"],
 		LastName:  data["last_name"],
 		Email:     data["email"],
+		RoleId:    2,
 	}
 
 	user.SetPassword(data["password"])
--- controllers/userController.go
+++ controllers/userController.go
@@ -11,7 +11,7 @@ func AllUsers(c *fiber.Ctx) error {
 
 	var users []models.User
 
-	database.DB.Find(&users)
+	database.DB.Preload("Role").Find(&users)
 
 	return c.JSON(users)
 }
@@ -39,7 +39,7 @@ func GetUser(c *fiber.Ctx) error {
 		Id: uint(id),
 	}
 
-	database.DB.Find(&user)
+	database.DB.Preload("Role").Find(&user)
 
 	return c.JSON(user)
 }
