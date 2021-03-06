4e3e1e68b616027f87f653b71b540aca99ffb6e6
Hiram G <hiramg90@gmail.com>
2021-03-06 08:52:38 -0600

    Updating user's profile & password

--- routes/routes.go
+++ routes/routes.go
@@ -15,6 +15,9 @@ func Setup(app *fiber.App) {
 	app.Post("/api/logout", controllers.Logout)
 	app.Get("/api/user", controllers.User)
 
+	app.Put("/api/users/info", controllers.UpdateInfo)
+	app.Put("/api/users/password", controllers.UpdatePassword)
+
 	app.Get("/api/users", controllers.AllUsers)
 	app.Post("/api/users", controllers.CreateUser)
 	app.Get("/api/users/:id", controllers.GetUser)
