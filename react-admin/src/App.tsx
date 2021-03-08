f925a17cc482630a5ccf66a02b8ff551127c5367
Hiram G <hiramg90@gmail.com>
2021-03-07 20:28:43 -0600

    Adding router

--- react-admin/src/App.tsx
+++ react-admin/src/App.tsx
@@ -1,67 +1,30 @@
 import React from 'react';
 import './App.css';
+import Nav from "./components/Nav";
+import Menu from "./components/Menu";
+import {BrowserRouter, Route} from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Users from "./pages/Users";
 
 function App() {
     return (
         <div className="App">
-            <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-                <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Company name</a>
-                <button className="navbar-toggler position-absolute d-md-none collapsed" type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false"
-                        aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <input className="form-control form-control-dark w-100" type="text" placeholder="Search"
-                       aria-label="Search"/>
-                <ul className="navbar-nav px-3">
-                    <li className="nav-item text-nowrap">
-                        <a className="nav-link" href="#">Sign out</a>
-                    </li>
-                </ul>
-            </header>
+            <BrowserRouter>
+                <Nav/>
 
-            <div className="container-fluid">
-                <div className="row">
-                    <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-                        <div className="position-sticky pt-3">
-                            <ul className="nav flex-column">
-                                <li className="nav-item">
-                                    <a className="nav-link active">
-                                        Dashboard
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </nav>
+                <div className="container-fluid">
+                    <div className="row">
+                        <Menu/>
 
-                    <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                        <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
 
-                        <div className="table-responsive">
-                            <table className="table table-striped table-sm">
-                                <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th>Header</th>
-                                    <th>Header</th>
-                                    <th>Header</th>
-                                    <th>Header</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <tr>
-                                    <td>1,001</td>
-                                    <td>random</td>
-                                    <td>data</td>
-                                    <td>placeholder</td>
-                                    <td>text</td>
-                                </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </main>
+                            <Route path={'/'} exact component={Dashboard}/>
+                            <Route path={'/users'} component={Users}/>
+
+                        </main>
+                    </div>
                 </div>
-            </div>
+            </BrowserRouter>
         </div>
     );
 }
---
+++ react-admin/src/components/Menu.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+const Menu = () => {
+    return (<nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+        <div className="position-sticky pt-3">
+            <ul className="nav flex-column">
+                <li className="nav-item">
+                    <Link className="nav-link" to={'/'}>
+                        Dashboard
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to={'/users'}>
+                        Users
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    </nav>);
+}
+
+export default Menu;
